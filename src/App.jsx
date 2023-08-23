cd305fb3df68f8e8da8457049069cc1330334377
jeunesseBurce <jdeburce@gmail.com>
2023-08-23 09:21:07 +0800

    Added new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import MainComponent from './components/MainComponent/MainComponent'
 import './App.css'
+import GameWindow from './components/GameWindow/GameWindow'
 
 function App() {
 
--- src/components/GameWindow/GameWindow.jsx
+++ src/components/GameWindow/GameWindow.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-
+import gameBackground from '../../assets/game-window-sample.png'
 
 function GameWindow() {
 
   return (
     <div className="game-window-layout">
-     
+       <img src={gameBackground} className="game-background" />
     </div>
   )
 }
