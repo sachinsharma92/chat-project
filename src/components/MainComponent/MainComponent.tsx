327d0c5441b22ed2f3af9c839306e71ece51cc26
jeunesseBurce <jdeburce@gmail.com>
2023-08-25 06:25:13 +0800

    Added html to image, and camera button functionality

--- src/components/MainComponent/MainComponent.tsx
+++ src/components/MainComponent/MainComponent.tsx
@@ -2,20 +2,16 @@ import InfoSidebar from '../InfoSidebar/InfoSidebar';
 import Avatar from '../Avatar/Avatar';
 import './MainComponent.css';
 import CameraButton from '../CameraButton/CameraButton';
-import MessageBox from '../MessageBox/MessageBox';
 
-function MainComponent() {
+function MainComponent({handleCameraButtonClick}: any) {
   return (
     <>
       <div className="main-component">
         <InfoSidebar />
         <div className="right-sidebar">
           <Avatar />
-          <CameraButton />
+          <CameraButton onButtonClick={handleCameraButtonClick} />
         </div>
-      </div>
-      <div className="footer">
-        <MessageBox />
       </div>
     </>
   );
