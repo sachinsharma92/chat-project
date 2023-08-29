57fbde06ed1d5c9cc8f5956b0ae40bd191687055
officialrobert <officialrobert.espina9627@gmail.com>
2023-08-29 22:29:15 +0800

    chore: setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
