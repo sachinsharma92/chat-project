4d1fa13695083dea2b0ba1c4c102ae43585ac60d
Robert Espina <officialrobert.espina9627@gmail.com>
2023-09-06 19:47:20 +0800

    fix: icons lint error (#16)

--- src/icons/ExploreIcon.tsx
+++ src/icons/ExploreIcon.tsx
@@ -13,16 +13,16 @@ const ExploreIcon = (props: SVGProps<SVGSVGElement>) => {
       <path
         d="M8 15C11.866 15 15 11.866 15 8C15 4.134 11.866 1 8 1C4.134 1 1 4.134 1 8C1 11.866 4.134 15 8 15Z"
         stroke="#BAB7A4"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M10.9679 5.03184L9.48386 9.48386L5.03184 10.9679L6.51584 6.51584L10.9679 5.03184Z"
         stroke="#BAB7A4"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
--- src/icons/SpacesIcon.tsx
+++ src/icons/SpacesIcon.tsx
@@ -11,13 +11,13 @@ const SpacesIcon = (props: SVGProps<SVGSVGElement>) => {
       {...props}
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M2.03903 4.62494C1.3899 5.07276 1 5.82666 1 6.63398V12.5854C1 13.919 2.04467 15 3.33333 15H12.6667C13.9553 15 15 13.919 15 12.5854V6.63398C15 5.82666 14.6101 5.07276 13.961 4.62494L9.2943 1.40553C8.51054 0.864825 7.48947 0.864825 6.7057 1.40553L2.03903 4.62494ZM7.99997 11.5C9.28864 11.5 10.3333 10.4554 10.3333 9.1667C10.3333 7.87803 9.28864 6.83337 7.99997 6.83337C6.71131 6.83337 5.66664 7.87803 5.66664 9.1667C5.66664 10.4554 6.71131 11.5 7.99997 11.5Z"
         stroke="#BAB7A4"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
