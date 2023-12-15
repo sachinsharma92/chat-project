cd8020e8029dc8912514ab30f77cef0cc31fb6fa
Robert Espina <officialrobert.espina9627@gmail.com>
2023-12-15 09:46:13 +0800

    feat: add google tag manager (#90)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={Inter.className}>
       <head>
+        <Script src="/google-tag-manager.js" />
+
         <meta name="theme-color" content="#f8f4e8" />
         <meta
           name="viewport"
@@ -27,6 +29,16 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <Script src="/twitter-pixel.js" strategy="lazyOnload" />
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WRLF43JJ"
+            height="0"
+            width="0"
+            className="invisible h-0 w-0"
+          ></iframe>
+        </noscript>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-KDQQYC94GB" />
+        <Script src="/gtag.js" />
         <div id="root">
           <ProvidersContainer>
             {children}
