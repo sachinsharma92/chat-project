caf9f71a4b803f9cb1773720baf6a4a7ac0170dc
sachinsharma92 <sachindesigner2018@gmail.com>
2024-01-04 20:42:50 +0530

    FIX: select issues

--- src/ui/features/GiftCard/index.tsx
+++ src/ui/features/GiftCard/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import Button from '@/components/common/Button';
 
-import './giftCard.css';
+import './GiftCard.css';
 
 interface GiftCardProps {
   closeHandler?: () => void;
@@ -53,6 +53,29 @@ const GiftCard: FC<GiftCardProps> = ({ closeHandler }) => {
             {items.btnText}
           </Button>
         ))}
+
+        {/* <div className="grid w-[10rem] grid-cols-1 gap-2 rounded-xl bg-gray-200 p-2">
+          <div>
+            <input type="radio" name="option" id="1" value="1" className="peer hidden" checked />
+            <label htmlFor="1" className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">1</label>
+          </div>
+
+          <div>
+            <input type="radio" name="option" id="2" value="2" className="peer hidden" />
+            <label htmlFor="2" className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">2</label>
+          </div>
+
+          <div>
+            <input type="radio" name="option" id="3" value="3" className="peer hidden" />
+            <label htmlFor="3" className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">3</label>
+          </div>
+
+          <div>
+            <input type="radio" name="option" id="4" value="3" className="peer hidden" />
+            <label htmlFor="4" className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white">4</label>
+          </div>
+        </div> */}
+
         <Button className="text-[22px] bg-white rounded-full font-bold h-[36px] mt-[74px] text-black">
           Pay
         </Button>
